344a0321ac414c7552259facd7f08df6de0a4f2d
kiranbhil <kiranbhil1997@gmail.com>
2022-12-14 20:36:55 +0530

    Home page commit

---
+++ front-end/src/Home/Footer.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { ReactNode } from 'react';
+import {
+  Box,
+  Container,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+function Footer() {
+  return (
+    <div>
+            
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container as={Stack} maxW={'6xl'} py={10}>
+        <SimpleGrid
+          templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 1fr 1fr' }}
+          spacing={8}>
+          <Stack spacing={6}>
+            <Box>
+              {/* <Logo color={useColorModeValue('gray.700', 'white')} /> */}
+            </Box>
+            <Text fontSize={'sm'}>
+              © 2022 Chakra Templates. All rights reserved
+            </Text>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Product</ListHeader>
+            <Link href={'#'}>Overview</Link>
+            <Link href={'#'}>Features</Link>
+            <Link href={'#'}>Tutorials</Link>
+            <Link href={'#'}>Pricing</Link>
+            <Link href={'#'}>Releases</Link>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Company</ListHeader>
+            <Link href={'#'}>About</Link>
+            <Link href={'#'}>Press</Link>
+            <Link href={'#'}>Careers</Link>
+            <Link href={'#'}>Contact</Link>
+            <Link href={'#'}>Partners</Link>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Support</ListHeader>
+            <Link href={'#'}>Help Center</Link>
+            <Link href={'#'}>Terms of Service</Link>
+            <Link href={'#'}>Legal</Link>
+            <Link href={'#'}>Privacy Policy</Link>
+            <Link href={'#'}>Status</Link>
+          </Stack>
+          <Stack align={'flex-start'}>
+            <ListHeader>Follow Us</ListHeader>
+            <Link href={'#'}>Facebook</Link>
+            <Link href={'#'}>Twitter</Link>
+            <Link href={'#'}>Dribbble</Link>
+            <Link href={'#'}>Instagram</Link>
+            <Link href={'#'}>LinkedIn</Link>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    </Box>
+
+    </div>
+  )
+}
+
+export default Footer
